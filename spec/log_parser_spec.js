3e6aae9d6b267437e76adf401cb4748f0eec2496
Kinsey Ashby <kinseyzaire@gmail.com>
2016-03-02 17:01:35 -0700

    making repo public

--- spec/log_parser_spec.js
+++ spec/log_parser_spec.js
@@ -16,13 +16,41 @@ describe('Parses dates from logs', function () {
       'I, [2014-05-10T14:48:19.093800 #85971]  INFO -- : innovate real-time'
     ]
 
-    expect(parser.prepLogs(logs)).toEqual(expected);
+    expect(parser.cutItUp(logs)).toEqual(expected);
   })
+  it('converts array of strings to a big obj with details', function(){
+     var expected = [
+        { date: '2014-05-10',
+       time: 'T14:48:19.093626',
+       type: 'D',
+       description: 'maximize' },
 
-  xit('extracts array of unique log dates', function () {
+       { date: '2014-05-11',
+        time: 'T14:48:19.093737',
+        type: 'I',
+        description: 'benchmark vertical' },
+
+        { date: '2014-05-10',
+         time: 'T14:48:19.093772',
+         type: 'D',
+         description: 'monetize turn-key' },
+
+         { date: '2014-05-12',
+         time: 'T14:48:19.093800',
+         type: 'I',
+         description: 'mars volta' },
+
+         { date: '2014-05-10',
+         time: 'T14:48:19.093800',
+         type: 'I',
+         description: 'innovate real-time' },
+     ]
+     expect(parser.makeMyBigAssAry(parser.cutItUp(logs))).toEqual(expected);
+ })
+  it('extracts array of unique log dates', function () {
     var expected = [ '2014-05-10', '2014-05-11', '2014-05-12' ]
 
-    expect(parser.getDates(logs)).toEqual(expected);
+    expect(parser.whutDayz(parser.makeMyBigAssAry(parser.cutItUp(logs)))).toEqual(expected);
   })
 
   xit('prints the list of log dates', function () {
